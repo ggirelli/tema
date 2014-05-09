8a8018cac1697ef025f37391431063619f7dfa01
ggirelli <gabriele@filopoe.it>
2014-05-09 11:21:07 +0200

    Built the basic structure of the SOGIsession class.

--- CJ/include/session.class.php
+++ CJ/include/session.class.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Manages SOGI sessions.
+ * 
+ * @author Gabriele Girelli <gabriele@filopoe.it>
+ * @copyright Gabriele Girelli 2014
+ * @version 0.0.1
+ */
 class SOGIsession {
 	
 	/**
@@ -42,10 +49,21 @@ class SOGIsession {
 	/* FUNCTIONS */
 	/*-----------*/
 
-	public function __construct() {
-		// Silence is golden!
+	/**
+	 * Loads a SOGIsession or creates a new one.
+	 */
+	public function __construct($id = null) {
+		if(is_null($id)) {
+			$this->init();
+		} else {
+			$this->load($id);
+		}
 	}
 
+	/**
+	 * Initialize a new SOGIsession in the current instance, if empty.
+	 * @return Boolean False if the current instance is not empty.
+	 */
 	public function init() {
 		if($this->id != -1) {
 			// Initialize a new session into the current instance
@@ -54,12 +72,51 @@ class SOGIsession {
 		}
 	}
 	
+	/**
+	 * Load a SOGIsession in the current instance, if empty, or returns a new SOGIsession class instance.
+	 * @param  int $id ID of the SOGIsession to load
+	 * @return SOGIsession     A new SOGIsession instance if the current one is not empty.
+	 */
 	public function load($id) {
 		if($this->id != -1) {
 			// Return a new SOGIsession instance
 		} else {
 			// Load into the current instance
 		}
+	}
+
+	/**
+	 * Retrieve the value of a parameter, in the current session.
+	 * @param  String $attr Attribute name
+	 * @return mix       The value of the attribute
+	 */
+	public function get($attr) {
+		// Return attribute value
+	}
+
+	/**
+	 * Retrieves the actual file list for the current session.
+	 * @return array List of file names
+	 */
+	private function getCurrFileList() {
+		// Return the current list of file names
+	}
+
+	/**
+	 * Writes a config file with all the session params.
+	 * @return Boolean T if success
+	 */
+	private function writeSession() {
+
+	}
+
+	/**
+	 * Reads a session from its config file
+	 * @param  int $id The ID of the session to read
+	 * @return array     Session attributes
+	 */
+	private function readSession($id) {
+
 	}
 
 }
