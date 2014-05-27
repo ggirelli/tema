7b93f9657381267fa844ed963544915d8114ff3e
ggirelli <gabriele@filopoe.it>
2014-05-27 08:37:43 +0200

    Added comments

--- CJ/SOGI-settings.php
+++ CJ/SOGI-settings.php
@@ -1,14 +1,23 @@
 <?php
 
+# CONSTANTS
+
 define('ROOT_URI', 'http://localhost/public_html/SOGI/CJ/');
 
-define('ROOT_PATH', './');
+define('ROOT_PATH', '/home/gire/public_html/SOGI/CJ/');
 define('CONT_PATH', ROOT_PATH . 'content/');
 define('INCL_PATH', ROOT_PATH . 'include/');
 define('SESS_PATH', ROOT_PATH . 'session/');
 
-# Array of filenames that cannot be uploaded
+# GLOBALS
+
+/**
+ * Array of filenames that cannot be uploaded
+ */
+global $FILENAME_BAN;
 $FILENAME_BAN = array('CONFIG', '.htaccess', 'php.ini');
+
+# REQUIRES
 
 require_once(INCL_PATH . 'functions.lib.php');
 require_once(INCL_PATH . 'session.class.php');
