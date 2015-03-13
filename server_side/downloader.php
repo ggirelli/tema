d58230d653975b0bc402676f89fa295fe4b579bf
ggirelli <gabriele@filopoe.it>
2015-03-13 07:21:18 +0100

    Fixed uploader behavior.

---
+++ server_side/downloader.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once('settings.php');
+require_once('include/tema.session.class.php');
+
+if ( isset($_GET['f']) and isset($_GET['s']) ) {
+	$s = new TEMAsession(HOST, USER, PWD, DB_NAME);
+	if ( $s->exists($_GET['s']) ) {
+
+		$file = SPATH . '/' . $_GET['s'] . '/' . $_GET['f'];
+		if ( file_exists($file) ) {
+
+			$quoted = sprintf('"%s"', addcslashes(basename($file), '"\\'));
+			$size   = filesize($file);
+
+			header('Content-Description: File Transfer');
+			header('Content-Type: application/octet-stream');
+			header('Content-Disposition: attachment; filename=' . $quoted); 
+			header('Content-Transfer-Encoding: binary');
+			header('Connection: Keep-Alive');
+			header('Expires: 0');
+			header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+			header('Pragma: public');
+			header('Content-Length: ' . $size);
+
+		}
+	}
+}
+
+die('ERROR');
+
+?>
