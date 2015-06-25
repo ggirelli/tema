bd469aeb31c54fab03e76e3abb571230c23adef8
ggirelli <gabriele@filopoe.it>
2015-06-25 15:53:08 +0200

    Fixed minor bug in db psql class.

--- server_side/include/db.class.psql.php
+++ server_side/include/db.class.psql.php
@@ -130,7 +130,7 @@ class C2SQL {
 	 * @return String    escaped
 	 */
 	protected function escape_string($s) {
-		return pg_escape__literal($this->psql, $s);
+		return pg_escape_literal($this->psql, $s);
 	}
 
 	// private FUNCTIONS
